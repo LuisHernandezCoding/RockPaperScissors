323f7c1e0816fd3cf24438a7e0c1610bc289ca59
LuisHernandezCoding <LuisHernandezCoding@gmail.com>
2022-09-18 16:41:41 -0500

    finished all the mechanics with UI integrated

--- javaScript.js
+++ javaScript.js
@@ -1,22 +1,187 @@
 
-let msjToPlayer
 let playerSelection
 let computerChoice
-let gameResult
+let matchResult
+let gameResult = ''
 let winner = ''
 let looser = ''
 let playerScore = '0'
 let computerScore = '0'
-let finalResult
+
+// Defining existing content
+const chalkboard = document.querySelector('#chalkboard')
+const topBar = chalkboard.firstElementChild
+const playerChoiceSquare = topBar.firstElementChild
+const title = playerChoiceSquare.nextElementSibling.firstElementChild
+const subtitle = title.nextElementSibling
+const computerChoiceSquare = topBar.lastElementChild
+const initialButtons = chalkboard.lastElementChild
+const btnStart = initialButtons.firstElementChild
+const btnCancel = initialButtons.lastElementChild
+
+// removing side Squares at the beginning
+topBar.removeChild(playerChoiceSquare)
+topBar.removeChild(computerChoiceSquare)
+
+// Adding listeners to existing buttons
+btnStart.addEventListener('click', StartGameUI)
+btnCancel.addEventListener('click', CancelGameUI)
+
+// Creating player choice buttons section
+const playerChoiceButtons = document.createElement('div')
+playerChoiceButtons.classList.add('buttons')
+const horizontalLine = document.createElement('div')
+horizontalLine.classList.add('horizontalLine', 'border')
+//      Creating rock Button
+const rockButton = document.createElement('button')
+rockButton.classList.add('rock', 'playerChoice')
+rockButton.addEventListener('click', () => startGame('Rock'))
+//      Creating paper Button
+const paperButton = document.createElement('button')
+paperButton.classList.add('paper', 'playerChoice')
+paperButton.addEventListener('click', () => startGame('Paper'))
+//      Creating Scissor Button
+const scissorsButton = document.createElement('button')
+scissorsButton.classList.add('scissors', 'playerChoice')
+scissorsButton.addEventListener('click', () =>  startGame('Scissors'))
+//      Sticking All Together
+playerChoiceButtons.appendChild(rockButton)
+playerChoiceButtons.appendChild(paperButton)
+playerChoiceButtons.appendChild(scissorsButton)
+
+// Creating Scoreboard 
+const scoreboard = document.createElement('div')
+scoreboard.classList.add('scoreboard')
+
+//      Creating player score elements
+const playerScoreSquare = document.createElement('div')
+const playerScoreTitle = document.createElement('div')
+const playerScoreValue = document.createElement('div')
+//      Giving player score classes
+playerScoreSquare.classList.add('playerScore')
+playerScoreTitle.classList.add('title')
+playerScoreValue.classList.add('value')
+//      Sticking all together
+playerScoreSquare.appendChild(playerScoreTitle)
+playerScoreSquare.appendChild(playerScoreValue)
+scoreboard.appendChild(playerScoreSquare)
+
+//      Creating divisor
+const divisor = document.createElement('div')
+divisor.classList.add('verticalLine', 'border')
+scoreboard.appendChild(divisor)
+
+//      Creating computer score elements
+const cpuScoreSquare = document.createElement('div')
+const cpuScoreSquareTitle = document.createElement('div')
+const cpuScoreSquareValue = document.createElement('div')
+//      Giving computer score classes
+cpuScoreSquare.classList.add('cpuScore')
+cpuScoreSquareTitle.classList.add('title')
+cpuScoreSquareValue.classList.add('value')
+//      Sticking all together
+cpuScoreSquare.appendChild(cpuScoreSquareTitle)
+cpuScoreSquare.appendChild(cpuScoreSquareValue)
+scoreboard.appendChild(cpuScoreSquare)
+updateScore()
+
+// Creating replay Button
+const replayButton = document.createElement('button')
+replayButton.classList.add('neutral')
+replayButton.textContent = 'Replay?'
+replayButton.addEventListener('click', replay)
+
+// Starting Game UI
+function StartGameUI(e){
+    title.textContent = 'Ok, Lets go!'
+    subtitle.textContent = 'Make a choice now!'
+    chalkboard.removeChild(initialButtons)
+    chalkboard.appendChild(playerChoiceButtons)
+    chalkboard.appendChild(horizontalLine)
+    chalkboard.appendChild(scoreboard)
+    topBar.insertBefore(playerChoiceSquare, topBar.firstElementChild)
+    topBar.appendChild(computerChoiceSquare)
+}
+// Cancel Game Function (before it starts) UI
+function CancelGameUI(e) {
+    title.textContent = 'Well, Take your time'
+    subtitle.textContent = 'and tell me when you are ready'
+    initialButtons.removeChild(btnCancel)
+    btnStart.classList.remove('authorize')
+    btnStart.classList.add('neutral')
+    btnStart.textContent = "I'm ready now!"
+    btnStart.removeEventListener('click', StartGameUI)
+    btnStart.addEventListener('click', UnCancelGameUI)
+}
+function UnCancelGameUI(e){
+    title.removeAttribute('class')
+    title.classList.add('title')
+    title.textContent = 'Welcome my friend!'
+    subtitle.textContent = 'Are you ready to play?'  
     
+    initialButtons.appendChild(btnCancel)
+    btnStart.classList.remove('neutral')
+    btnStart.classList.add('authorize')
+    btnStart.textContent = "Let's Go!"
+    btnStart.removeEventListener('click', UnCancelGameUI)
+    btnStart.addEventListener('click', StartGameUI)  
+
+}
+// Update Score
+function updateScore(){
+    //      Giving player score values
+    playerScoreTitle.textContent = 'Player Score:'
+    playerScoreValue.textContent = playerScore
+    //      Giving computer score values
+    cpuScoreSquareTitle.textContent = 'Computer Score:'
+    cpuScoreSquareValue.textContent = computerScore
+
+    // updating SideSquares
+
+    playerChoiceSquare.lastElementChild.removeAttribute('class')
+    playerChoiceSquare.lastElementChild.classList.add('playerChoice')
+    if (playerSelection != undefined && playerSelection != '') playerChoiceSquare.lastElementChild.classList.add(playerSelection.toLowerCase())
+    
+    // updating SideSquares
+    computerChoiceSquare.lastElementChild.removeAttribute('class')
+    computerChoiceSquare.lastElementChild.classList.add('playerChoice')
+    if (computerChoice != undefined && computerChoice != '') computerChoiceSquare.lastElementChild.classList.add(computerChoice.toLowerCase())
+    
+    if(playerScore >= 5 || computerScore >= 5) finishGame()
+}
+// Start playing the game
+function startGame (playerChoice){
+    computerChoice = getComputerChoice()
+    playerSelection = playerChoice
+    matchResult = GetWinner(playerSelection, computerChoice)
+    setWinnerAndLooserSelection(matchResult, playerSelection, computerChoice)
+
+    title.removeAttribute('class')
+    title.classList.add('title')
+    if (matchResult === 'Win') {
+        playerScore++
+        title.classList.add('win')
+    }
+    else if (matchResult === 'Loose') {
+        computerScore++
+        title.classList.add('loose')
+    }
+    else title.classList.add('draw')
+
+    title.textContent = 'You ' + matchResult + '!';
+    if (matchResult === 'Draw') subtitle.textContent = 'A ' + winner + ' draws with a ' + winner
+    else subtitle.textContent = winner + ' beats ' + looser
+    updateScore()
+}
+// Function to get the choice from the computer
 function getComputerChoice(){
     let choice
     let pickedNumber = (Math.floor(Math.random()*100) + 1)
     return (pickedNumber >= 67) ? choice = 'Rock' : 
     (pickedNumber <= 33) ? choice = 'Paper' : 'Scissors'
 }
-
-function Play(playerSelection, computerSelection){
+// Compare decisions made and return the result
+function GetWinner(playerSelection, computerSelection){
     return (playerSelection === 'Rock' && 'Scissors' === computerSelection ||
     playerSelection === 'Paper' && 'Rock' === computerSelection ||
     playerSelection === 'Scissors' && 'Paper' === computerSelection) ? 'Win' :
@@ -25,8 +190,8 @@ function Play(playerSelection, computerSelection){
     playerSelection === 'Scissors' && 'Rock' === computerSelection) ? 'Loose' :
     'Draw';
 }
-
-function setWinnerAndLooser(endState, playerSelection, computerSelection){
+// This is for make the winner and looser selections clear for displaying it
+function setWinnerAndLooserSelection(endState, playerSelection, computerSelection){
     if (endState === 'Win'){
         winner = playerSelection
         looser = computerSelection
@@ -39,127 +204,44 @@ function setWinnerAndLooser(endState, playerSelection, computerSelection){
         winner = playerSelection
         looser = playerSelection
     }
-
 }
-function game(gamesQuantity){
-    for (let i = 0; i < gamesQuantity; i++){
-        msjToPlayer = (i === 0) ? 'Make your selection player' : 
-        'Round ' + (i + 1) + ' Make your selection player, last round you ' + gameResult + ' with a ' + playerSelection
-        let playerPrompt = prompt(msjToPlayer)
-        playerSelection = playerPrompt.toString().charAt(0).toUpperCase() + playerPrompt.slice(1).toLowerCase();
-   
-        while (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection && 'Scissors'){
-            playerPrompt = prompt('WRONG INPUT... ' + msjToPlayer)
-            playerSelection = playerPrompt.toString().charAt(0).toUpperCase() + playerPrompt.slice(1).toLowerCase();
-        }
-
-        computerChoice = getComputerChoice()
-        gameResult = Play(playerSelection, computerChoice)
-        setWinnerAndLooser(gameResult, playerSelection, computerChoice)
-
-        if (gameResult === 'Win') playerScore++
-        else if (gameResult === 'Loose') computerScore++
-
-        let endString = 'Round ' + (i + 1) + ': ' +  'You ' + gameResult + '!';
-        if (gameResult === 'Draw') endString += ', a ' + winner + ' never beat a ' + winner
-        else endString += ', ' + winner + ' beats ' + looser
-
-        console.log(endString)  
+// This is reached when some score hits 5
+function finishGame(){
+    title.removeAttribute('class')
+    title.classList.add('title')
+    if(playerScore >= 5) {
+        title.textContent = 'Congratulations!'
+        title.classList.add('win')
+        subtitle.textContent = 'You are: the winner'
     }
-    console.log('Final Score:')
-    console.log('Player: ' + playerScore + ' - Computer: ' + computerScore)
-    finalResult = (playerScore > computerScore) ? 'Win' :
-                  (playerScore < computerScore) ? 'Loose' : 'Draw'
-    console.log(finalResult)
+    else{
+        title.textContent = 'Too Bad!'
+        title.classList.add('loose')
+        subtitle.textContent = 'You are: the looser'
+    }
+    chalkboard.removeChild(playerChoiceButtons)
+    chalkboard.removeChild(horizontalLine)
+    chalkboard.appendChild(replayButton)
+    topBar.removeChild(playerChoiceSquare)
+    topBar.removeChild(computerChoiceSquare)
 }
-
-// Defining existing content
-const chalkboard = document.querySelector('#chalkboard')
-const title = chalkboard.firstElementChild
-const initialButtons = chalkboard.lastElementChild
-const btnStart = initialButtons.firstElementChild
-const btnCancel = initialButtons.lastElementChild
-
-// Adding listeners to existing buttons
-btnStart.addEventListener('click', StartGame)
-btnCancel.addEventListener('click', CancelGame)
-
-// Creating player choice buttons section
-const playerChoiceButtons = document.createElement('div')
-playerChoiceButtons.classList.add('buttons')
-const rockButton = document.createElement('button')
-rockButton.classList.add('rock', 'playerChoice')
-const paperButton = document.createElement('button')
-paperButton.classList.add('paper', 'playerChoice')
-const scissorsButton = document.createElement('button')
-scissorsButton.classList.add('scissors', 'playerChoice')
-playerChoiceButtons.appendChild(rockButton)
-playerChoiceButtons.appendChild(paperButton)
-playerChoiceButtons.appendChild(scissorsButton)
-const horizontalLine = document.createElement('div')
-horizontalLine.classList.add('horizontalLine', 'border')
-
-// Creating Scoreboard 
-const scoreboard = document.createElement('div')
-scoreboard.classList.add('scoreboard')
-//      Creating player score elements
-const playerScoreSquare = document.createElement('div')
-const playerScoreTitle = document.createElement('div')
-const playerScoreValue = document.createElement('div')
-//      Giving player score classes
-playerScoreSquare.classList.add('playerScore')
-playerScoreTitle.classList.add('title')
-playerScoreValue.classList.add('value')
-//      Giving player score values
-playerScoreTitle.textContent = 'Player Score:'
-playerScoreValue.textContent = playerScore
-//      Sticking all together
-playerScoreSquare.appendChild(playerScoreTitle)
-playerScoreSquare.appendChild(playerScoreValue)
-scoreboard.appendChild(playerScoreSquare)
-
-//      Creating computer score elements
-const cpuScoreSquare = document.createElement('div')
-const cpuScoreSquareTitle = document.createElement('div')
-const cpuScoreSquareValue = document.createElement('div')
-//      Giving computer score classes
-cpuScoreSquare.classList.add('cpuScore')
-cpuScoreSquareTitle.classList.add('title')
-cpuScoreSquareValue.classList.add('value')
-//      Giving computer score values
-cpuScoreSquareTitle.textContent = 'Computer Score:'
-cpuScoreSquareValue.textContent = computerScore
-//      Sticking all together
-cpuScoreSquare.appendChild(cpuScoreSquareTitle)
-cpuScoreSquare.appendChild(cpuScoreSquareValue)
-scoreboard.appendChild(cpuScoreSquare)
-
-// Starting Game Function
-function StartGame(e){
-    title.textContent = 'Ok, Lets go! Make a choice now!'
-    chalkboard.removeChild(initialButtons)
-    chalkboard.appendChild(playerChoiceButtons)
-    chalkboard.appendChild(horizontalLine)
-    chalkboard.appendChild(scoreboard)
-}
-
-// Function for restarting initial buttons
-function RestartInitialButtons(e) {
-    initialButtons.appendChild(btnCancel)
-    btnStart.classList.remove('neutral')
-    btnStart.classList.add('authorize')
-    btnStart.textContent = "Let's Go!"
-    title.textContent = 'Are you ready to play?'
-    btnStart.removeEventListener('click', RestartInitialButtons)
-    btnStart.addEventListener('click', StartGame)
-}
-// Cancel Game Function (before it starts)
-function CancelGame(e) {
-    title.textContent = 'Well, Take your time'
-    initialButtons.removeChild(btnCancel)
-    btnStart.classList.remove('authorize')
-    btnStart.classList.add('neutral')
-    btnStart.textContent = "I'm ready now!"
-    btnStart.removeEventListener('click', StartGame)
-    btnStart.addEventListener('click', RestartInitialButtons)
+// Play again function
+function replay() {
+    chalkboard.removeChild(replayButton)
+    chalkboard.removeChild(scoreboard)
+    chalkboard.appendChild(initialButtons)
+    // Restarting variables
+    title.removeAttribute('class')
+    title.classList.add('title')
+    title.textContent = 'Welcome my friend!'
+    subtitle.textContent = 'Are you ready to play?' 
+    playerSelection = ''
+    computerChoice = ''
+    matchResult = ''
+    gameResult = ''
+    winner = ''
+    looser = ''
+    playerScore = '0'
+    computerScore = '0'
+    updateScore()
 }
